81b953133fb2e393454cd04f8c18c0dac6bcea83
Benedicta-Asare <lilbengilla@gmail.com>
2023-03-25 00:22:21 +0000

    exercise_1

---
+++ Exercise/Exercise_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    
+    // gallons to cubic feet conversion
+    int num_of_gallons;
+    const float gallons = 7.481;
+    float result;
+    cout << "Enter number of gallons: ";
+    cin >> num_of_gallons;
+    result = num_of_gallons / gallons;
+    cout << "The equivalent value in cubic feet is " << result << endl;
+    
+
+
+    //converting temperature in degrees Celsius to degrees Fahrenheit
+    float tempc, tempf;
+    cout << "\nEnter temperature in degrees Celsius: ";
+    cin >> tempc;
+    tempf = (tempc * 1.8) + 32;
+    cout << "The equivalent temperature in degrees Fahrenheit is " << tempf << endl;
+
+    return 0;
+}
