fdd6a96643c5dc4724bd37a9fbe9b845a2018bd6
Benedicta-Asare <lilbengilla@gmail.com>
2023-03-30 22:52:54 +0000

    create Exercise_3b

---
+++ Exercise/Exercise_3b.cpp
@@ -0,0 +1,26 @@
+//Function that sets the smaller value to zero
+#include <iostream>
+using namespace std;
+
+void zeroSmaller(int &, int &);
+
+int main(){
+    int x, y;
+    cout << "Enter First Value: ";
+    cin >> x;
+    cout << "Enter Second Value: ";
+    cin >> y;
+
+    zeroSmaller(x ,y);
+
+    cout << "First Number is now " << x << endl;
+    cout << "Second Number is now " << y << endl;
+}
+void zeroSmaller(int &a, int &b){
+    if (a<b){
+        a = 0;
+    }
+    else{
+        b = 0;
+    }
+}
