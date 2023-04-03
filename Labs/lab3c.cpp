39f68f3673a62d60e1c65571f8e7240ab7af28b2
Benedicta-Asare <lilbengilla@gmail.com>
2023-04-03 20:16:39 +0000

    update lab3c

--- Labs/lab3c.cpp
+++ Labs/lab3c.cpp
@@ -1,9 +1,9 @@
-//function to generate the Fibonacci sequence up to a given number
+//Function to generate the Fibonacci sequence up to a given number
 #include <iostream>
 using namespace std;
 
 void fibonacci(int n){
-    int num1 = 0,num2 = 1,next_term = 0;
+    int num1 = 0, num2 = 1, next_term = 0;
     
     cout << "Fibonacci sequence: " << num1 << "," << num2;
 
