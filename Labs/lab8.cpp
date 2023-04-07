7c689eeafe3758da65924391589f6e90dd85282b
Benedicta-Asare <lilbengilla@gmail.com>
2023-04-07 23:05:48 +0000

    update lab8

--- Labs/lab8.cpp
+++ Labs/lab8.cpp
@@ -8,20 +8,20 @@ public:
  void set_values(int a , int b){
     width = a, height = b;
  }
-    virtual int area(){
+    virtual int area(void){
         return 0;
 }
 };
 
 class CRectangle : public CPolygon
-{
+{public:
     int area(){
         return width * height;
     }
 };
 
 class CTriangle : public CPolygon
-{
+{public:
     int area(){
         return width * height / 2;
     }
@@ -32,16 +32,16 @@ int main(){
     CTriangle trgl;
     CPolygon poly;
 
-    CRectangle* ppoly1 = &rect;
-    CTriangle* ppoly2 = &trgl;
-    CPolygon* ppoly3 = &poly;
+    CRectangle *ppoly1 = &rect;
+    CTriangle *ppoly2 = &trgl;
+    CPolygon *ppoly3 = &poly;
 
-    ppoly1 -> set_values(10,5);
-    ppoly2 -> set_values(10,5);
-    ppoly3 -> set_values(10,5);
+    ppoly1-> set_values(10,5);
+    ppoly2-> set_values(10,5);
+    ppoly3-> set_values(10,5);
 
-    ppoly1 -> area();
-    ppoly2 -> area();
-    ppoly3 -> area();
+    cout<<ppoly1-> area()<<endl;
+    cout<<ppoly2-> area()<<endl;
+    cout<<ppoly3-> area()<<endl;
     return 0;
 }
